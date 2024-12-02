629a309c6099e3e88eec4ef77ae507749e9f32f6
TheJ0lly <eusicumine7@gmail.com>
2024-12-02 19:52:28 +0200

    Removed redundant code.

--- include/widgets/label.hpp
+++ include/widgets/label.hpp
@@ -7,9 +7,6 @@
 namespace SUI::Widget {
     class Label : public BaseWidget {
         public:
-            /*
-            `padding` - if you set it to a non-zero value, it will draw a rectangle around the label text.
-            */
             Label(const char *text, f32 x, f32 y, HighlightType ht);
             ~Label();
 
